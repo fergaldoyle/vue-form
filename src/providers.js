3431f38295df590ac71d60637c61454254d3f97e
fergaldoyle <fergal_doyle@yahoo.com>
2017-04-11 21:19:35 +0100

    remove underscore

--- src/providers.js
+++ src/providers.js
@@ -1,5 +1,5 @@
 import { randomId } from './util';
 
 const s = typeof Symbol === 'function';
-export const vueFormConfig = s ? Symbol() : `VueFormProviderConfig_${randomId}`;
+export const vueFormConfig = s ? Symbol() : `VueFormProviderConfig${randomId}`;
 export const vueFormState =  s ? Symbol() : `VueFormProviderState${randomId}`;
